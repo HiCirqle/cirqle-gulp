ecd1dc2a0d8f3d0d4fd856d488920c58dc1a2ff0
Jon Bolt <epic.bagel@gmail.com>
2015-05-12 11:37:14 +0100

    Minifying is broke

--- tasks/build.js
+++ tasks/build.js
@@ -6,14 +6,13 @@ module.exports = function(gulp, $, config, patterns) {
             var assets = $.useref.assets({
                 searchPath: '.tmp'
             });
-            console.log([config.root, '.tmp'].join('/'), [config.root, 'dist'].join('/'))
             return gulp.src('.tmp/index.html')
                 .pipe(assets) // Get assets from index.html
                 .pipe($.rev()) // Apply version names to built files
                 /*.pipe($.if('*.js', $.uglify({ // concat and uglify js
                     mangle: false // Do not mangle variable names for Angular
                 })))*/
-                .pipe($.if('*.css', $.minifyCss())) // Minify css
+                //.pipe($.if('*.css', $.minifyCss())) // Minify css
                 .pipe(assets.restore()) // Rewrite unversioned filerefs in index.html
                 .pipe($.revReplace({
                     prefix: '/'
