868641fa9448c0fe25fe5c376bd704104a38e147
brandonweis <kokweiong@gmail.com>
2015-09-14 20:54:27 +0800

    add jshint fail reporter

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,9 +1,22 @@
 'use strict';
+var stylish = require('jshint-stylish'),
+    _ = require('underscore'),
+    fs = require('fs');
+var packageJSON  = require('../package');
+var jshintConfig = packageJSON.jshintConfig;
+jshintConfig.lookup = false;
 
 module.exports = function(gulp, $, config, patterns) {
+    var jshintrc = JSON.parse(fs.readFileSync('./.jshintrc', 'utf8')) || {};
+    jshintConfig = _.extend(jshintConfig, jshintrc);
+
     return function() {
         gulp.task('scripts', function() {
             return gulp.src('app/scripts/**/*.js')
+                .pipe($.jshint(jshintConfig)) // jshint test
+                // .pipe($.jshint.reporter('default', { verbose: true })) // reporter
+                .pipe($.jshint.reporter(stylish)) // reporter
+                .pipe($.jshint.reporter('fail')) // jshint test stop when failed
                 .pipe($.plumber())
                 .pipe($.cached('scripts'))
                 .pipe($.es6Transpiler({
