9b9d54045fa77f70f8c184bc660483fd34cfc2fd
Jon Bolt <epic.bagel@gmail.com>
2015-09-18 15:02:36 +0100

    Production checks for analytics

--- tasks/html.js
+++ tasks/html.js
@@ -3,14 +3,21 @@
 module.exports = function(gulp, $, config, patterns) {
     return function() {
         if (config.env === 'development' || require('yargs').argv.livereload) {
-            console.log('Setting live reload')
+            console.log('Setting live reload');
         }
-        gulp.task('html', function () {
+        gulp.task('html', function() {
             return gulp.src('app/index.' + config.templateExt)
                 // Use jade for the templates 
                 .pipe($.if(config.templateExt === 'jade', $.jade({
                     pretty: true
                 })))
+                // Do anything conditional in the templates
+                .pipe($.preprocess({
+                    context: {
+                        NODE_ENV: process.env.NODE_ENV,
+                    }
+                }))
+                // Inject JS
                 .pipe($.inject(gulp.src('./app/scripts/**/*.js', {
                     read: false
                 }), {
