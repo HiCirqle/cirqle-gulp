4658947f7c55a07c749709ac7f11175ddcabf97b
Remco Moolenaar <remco@saskiaenremco.nl>
2017-07-05 14:58:38 +0200

    Fix

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -3,7 +3,7 @@
 module.exports = function(gulp, $, config, patterns) {
     return function() {
         gulp.task('fonts', function() {
-            return gulp.src('app/fonts/**/*.{eot,svg,ttf,wof,woff2}')
+            return gulp.src('app/fonts/**/*.{eot,svg,ttf,woff,woff2}')
                 .pipe(gulp.dest('.tmp/fonts'))
         });
     }
